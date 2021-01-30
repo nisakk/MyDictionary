f9764395ac28749dc7f675ab4162bd8a22ce49df
nisa küçükkömürcü <nisakucukkomurcu@gmail.com>
2021-01-30 17:34:33 +0300

    Add project files.

---
+++ MyDictionary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+	class MyDictionary<T,N>
+	{
+		T[] keyArray;
+		N[] valueArray;
+
+		public MyDictionary()
+		{
+			keyArray = new T[0];
+			valueArray = new N[0];
+		}
+
+		public void Add(T key, N value)
+		{
+			T[] tempKey = keyArray;
+			N[] tempValue = valueArray;
+			keyArray = new T[keyArray.Length + 1];
+			valueArray = new N [valueArray.Length + 1];
+			
+			for (int i = 0; i < tempKey.Length; i++)
+			{
+				keyArray[i] = tempKey[i];	
+			}
+
+			for (int i = 0; i < tempValue.Length; i++)
+			{
+				valueArray[i] = tempValue[i];
+			}
+
+			keyArray[keyArray.Length - 1] = key;
+			valueArray[valueArray.Length - 1] = value;
+
+			Console.WriteLine("Öğrenci No: " + key + " " + value);
+
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyDictionary
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			MyDictionary<int, string> ogrenci = new MyDictionary<int, string>();
+
+			ogrenci.Add(101, "Ayşe Yaman");
+			ogrenci.Add(102, "Ahmet Güzel");
+			ogrenci.Add(103, "Meyrem Uzerli");
+			ogrenci.Add(104, "Neslihan Atagül");
+			ogrenci.Add(105, "Yasemin Ergene");
+			ogrenci.Add(106,"Burak Özçivit");
+
+		}
+	}
+}
